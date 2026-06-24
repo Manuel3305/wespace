7c8afe74cf2647545e65aaf7c8b53f410af47532
Manuel Falter <manuel3305@MacBook-Pro-von-Manuel.local>
2026-06-24 03:20:42 +0200

    Fix PWA install mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'leaflet/dist/leaflet.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
+import "./index.css";
+import App from "./App.jsx";
+import "leaflet/dist/leaflet.css";
 
-createRoot(document.getElementById('root')).render(
+registerSW({
+  immediate: true,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
